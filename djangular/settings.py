67275364ce2ccd3fbec1a730db0da97e23d9d2a0
tishad <imtishad@gmail.com>
2016-12-03 20:35:31 +0600

    script link fixes

--- djangular/settings.py
+++ djangular/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
